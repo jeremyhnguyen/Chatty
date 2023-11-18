cda78ba050b963518996e1b02fead3fda73a2bae
Jeremy Nguyen <jeremycnguyen@gmail.com>
2023-11-18 01:37:29 +0000

    updated .env for vite

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,8 +1,6 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL =
-  // eslint-disable-next-line no-undef
-  process.env.NODE_ENV === "production" ? undefined : "http://localhost:8081";
+const URL = import.meta.env.PROD ? undefined : "http://localhost:8081";
 
 export const socket = io(URL);
