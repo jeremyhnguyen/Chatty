785234287fc4f612aef521a9bfc460e079c70fdb
Jeremy Nguyen <jeremycnguyen@gmail.com>
2023-12-07 02:15:11 +0000

    added port to url on App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,7 @@ import { Routes, Route } from "react-router-dom";
 import { AppContext, type Theme } from "./components/AppContext";
 import { User, Auth } from "./api";
 
-const socket = io("chatty-dev.us-west-2.elasticbeanstalk.com", {
+const socket = io("chatty-dev.us-west-2.elasticbeanstalk.com:8081", {
   path: "/api/socket.io/",
 });
 
