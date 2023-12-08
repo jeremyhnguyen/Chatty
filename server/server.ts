35e15a6c8dd5556e0fc927f3382b2c182f9c2199
Jeremy Nguyen <jeremycnguyen@gmail.com>
2023-12-08 22:21:28 +0000

    Added beanstalk url to both client and server

--- server/server.ts
+++ server/server.ts
@@ -37,6 +37,7 @@ const app = express();
 const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
+  path: 'http://chatty-dev.us-west-2.elasticbeanstalk.com/',
   cors: {
     origin: '*',
     // origin: [
