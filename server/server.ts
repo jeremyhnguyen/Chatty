575205f5f092433e7dcf75fbb181d1426d335942
Jeremy Nguyen <jeremycnguyen@gmail.com>
2023-12-08 22:24:49 +0000

    Added port number url in client and server

--- server/server.ts
+++ server/server.ts
@@ -37,7 +37,7 @@ const app = express();
 const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
-  path: 'http://chatty-dev.us-west-2.elasticbeanstalk.com/',
+  path: 'http://chatty-dev.us-west-2.elasticbeanstalk.com:8081',
   cors: {
     origin: '*',
     // origin: [
